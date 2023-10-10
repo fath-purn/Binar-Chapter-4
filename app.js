9343cdec84f22e307a220b7c884b7a4c27b31b93
fathpurn <talitakamilia345@gmail.com>
2023-10-10 22:54:54 +0700

    feat: buat database

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT = 3000 } = process.env;
 
-const v1Router = require("./routes/v1") 
+const v1Router = require("./routes/v1/endPointV1") 
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/v1', v1Router);
+
 
 
 app.listen(PORT, () => {
