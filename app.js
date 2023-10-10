23d3dd4255ed8f5e9f2c695512d10bdc188b603e
fathpurn <talitakamilia345@gmail.com>
2023-10-10 19:56:54 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT = 3000 } = process.env;
+
+const v1Router = require("./routes/v1") 
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api/v1', v1Router);
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
